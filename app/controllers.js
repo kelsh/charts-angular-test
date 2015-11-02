42d58e6bf71e4cb4e6b3c6a6d2f08ceeebb9675e
pstrawberie <pjoshhh@gmail.com>
2015-11-01 23:03:48 -0700

    integrating angular

--- app/controllers.js
+++ app/controllers.js
@@ -1,8 +1,75 @@
-$(function() {
-    /*$('#container').highcharts({
-       
-    });*/
-    Chart.defaults.global.pointHitDetectionRadius = 1;
+var chartsExample = angular.module('chartsExample', []);
+
+chartsExample.controller('chartsCtrl', function ($scope) {
+
+	var hiData={
+		 chart: {
+            type: 'bar'
+        },
+        title: {
+            text: 'Fruit Consumption'
+        },
+        xAxis: {
+            categories: ['Apples', 'Bananas', 'Oranges']
+        },
+        yAxis: {
+            title: {
+                text: 'Fruit eaten'
+            }
+        },
+        series: [{
+            name: 'Jane',
+            data: [1, 0, 4]
+        }, {
+            name: 'John',
+            data: [5, 7, 3]
+        }]
+	}
+
+  var chartsList = {
+   highCharts : {
+  	
+  	drawChart: function  (el, data) {
+  		var data = data;
+  		var target = jQuery(el);
+  		target.highcharts(data);
+  	}
+  },
+  cThree: {
+  	drawCThree:function(){
+  		var ctx1 = document.getElementById("chart1").getContext("2d");
+        window.myLine = new Chart(ctx1).Line(lineChartData, {
+            showScale: false,
+            pointDot: true,
+            responsive: true
+        });
+        var ctx2 = document.getElementById("chart2").getContext("2d");
+        window.myLine = new Chart(ctx2).Line(lineChartData, {
+            responsive: true
+        });
+        var chart = c3.generate({
+            bindto: '#chart',
+            data: {
+                columns: [
+                    ['data1', 30, 200, 100, 400, 150, 250],
+                    ['data2', 50, 20, 10, 40, 15, 25]
+                ],
+                axes: {
+                    data2: 'y2' // ADD
+                }
+            },
+            axis: {
+                y2: {
+                    show: true // ADD
+                }
+            }
+        });
+    }
+  	
+  },
+  chartJs: {
+  	drawChartJs:function(){
+  		Chart.defaults.global.pointHitDetectionRadius = 1;
     Chart.defaults.global.customTooltips = function(tooltip) {
         var tooltipEl = $('#chartjs-tooltip');
         if (!tooltip) {
@@ -52,33 +119,11 @@ $(function() {
             data: [randomScalingFactor(), randomScalingFactor(), randomScalingFactor(), randomScalingFactor(), randomScalingFactor(), randomScalingFactor(), randomScalingFactor()]
         }]
     };
-    window.onload = function() {
-        var ctx1 = document.getElementById("chart1").getContext("2d");
-        window.myLine = new Chart(ctx1).Line(lineChartData, {
-            showScale: false,
-            pointDot: true,
-            responsive: true
-        });
-        var ctx2 = document.getElementById("chart2").getContext("2d");
-        window.myLine = new Chart(ctx2).Line(lineChartData, {
-            responsive: true
-        });
-        var chart = c3.generate({
-            bindto: '#chart',
-            data: {
-                columns: [
-                    ['data1', 30, 200, 100, 400, 150, 250],
-                    ['data2', 50, 20, 10, 40, 15, 25]
-                ],
-                axes: {
-                    data2: 'y2' // ADD
-                }
-            },
-            axis: {
-                y2: {
-                    show: true // ADD
-                }
-            }
-        });
-    };
+  	}
+  }
+  };
+
+  chartsList.highCharts.drawChart("#container", hiData);
+  chartsList.chartJs.drawChartJs();
+  chartsList.cThree.drawCThree();
 });
